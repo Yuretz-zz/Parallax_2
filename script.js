2bb87ca7f758269d0a6813f22520868649a90aa5
Yuretz-zz <113853077+Yuretz-zz@users.noreply.github.com>
2023-06-25 19:50:03 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ document.addEventListener('touchmove', (e) => {
         `
     })
 })
+
+document.addEventListener('mousemove', (e) => {
+
+    Object.assign(document.documentElement, {
+        style: `
+            --move-x: ${( e.clientX - window.innerWidth / 2 ) * -.01}deg;
+            --move-y: ${( e.clientY - window.innerHeight / 2) * .02}deg;
+        `
+    })
+})
+
+let a = 0;
+function move() {
+    a = Math.round( Math.random() * 40 );
+    document.querySelector('.test').style.transform = `rotateZ(${a}deg)`;
+    setTimeout(move, 2000)
+}
+
+// move();
